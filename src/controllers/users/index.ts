3f6c7230fd28621d45a6b2117752174554bdc73a
Sebastián <s.sameghini@gmail.com>
2025-01-25 17:33:21 -0300

    Remove unnecessary controller

--- src/controllers/users/index.ts
+++ src/controllers/users/index.ts
@@ -5,10 +5,4 @@ const router = express.Router();
 
 router.post('/', registerUser);
 
-router.get('/:id', (req, res) => {
-    const userId = req.params.id;
-    // Logic to get a specific user by ID
-    res.send(`User with ID: ${userId}`);
-});
-
 export default router;
