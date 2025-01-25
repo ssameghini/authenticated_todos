10bcae2a2dd92a5cc0c5ef6b04ed1519dd841d3f
Sebastián <s.sameghini@gmail.com>
2025-01-25 17:35:59 -0300

    Complete routes

--- src/routes.ts
+++ src/routes.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import userRoutes from './controllers/users';
+import todoRoutes from './controllers/toDos';
+import authRoutes from './controllers/auth';
 
 const router = express.Router();
 
+router.use('/auth', authRoutes);
 router.use('/users', userRoutes);
+router.use('/todos', todoRoutes);
 
 export default router;
